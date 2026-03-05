99ed13e5dcdb12aa549bb8dd607794c6728556e9
HamptonJake <hamptonjake23@gmail.com>
2026-03-05 17:35:34 -0500

    adding the pointers file

---
+++ pointy.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+
+
+
+// memory and variables
+
+
+
+
+
+void main(){
+
+
+  // determine how many bytes of memory a certain type uses
+
+  printf("An int uses %zu bytes of memory\n", sizeof(int));
+  printf("A double uses %zu bytes of memory\n", sizeof(float));
+
+
+  int x = 10;
+  int y = 100;
+
+  float a = 1.2;
+  float b = 25.1;
+  
+  printf("An int expression uses %zu\n", sizeof(y-x));
+  printf("A float expression uses %zu\n", sizeof(b-a));
+
+
+
+  // how is memory stored?
+
+
+  /* each byte of memory is identified by an integer
+     when you have a data type such as an int that uses more than one byte of memory, the bytes that make up the data are always adjacent to one another in memory
+     sometimes these are in order, but not always
+  */
+
+
+  // what is a pointer?
+
+  // a pointer is a variable that holds an address
+  // just like an int variable can hold the value 12, a pointer variable can hold the address of data
+
+  // used interchangeably when referring to pointers: index into memory, address, location
+
+
+
+
+  
+}
