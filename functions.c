2284f58e33d1797a3f0bdbeb4a55211ba38f62a1
HamptonJake <hamptonjake23@gmail.com>
2026-03-05 17:10:53 -0500

    added some function prototypes

--- functions.c
+++ functions.c
@@ -2,9 +2,22 @@
 
 
 
+// function prototypes
+// Notify the compiler in advance that you'll be using the function of a certain type
+void foo(void);
+
+
+
 int plus_one(int n){
   return n+1;
 }
+
+
+void secret_message(void){
+  printf("This is a secret message\n");
+}
+
+
 
 
 // Arguments are copied into the parameters
@@ -19,7 +32,18 @@ int main(void){
 
   printf("i + 1 is %d\n", j);
 
+  secret_message();
+
+  foo();
+
+  
 
 
+  
 
+}
+
+
+void foo(void){
+  printf("This was declared after the main function\n");
 }
