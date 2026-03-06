cfb3f953b6c62418ebf7c2715157a8288032d5f2
HamptonJake <hamptonjake23@gmail.com>
2026-03-06 15:58:45 -0500

    pointers in a print statement

--- pointy.c
+++ pointy.c
@@ -46,6 +46,9 @@ void main(){
   // used interchangeably when referring to pointers: index into memory, address, location
 
 
+  int i = 10;
+  printf("The value of i is %d\n", i);
+  printf("The address of i is %p\n", (void*)&i);
 
 
   
